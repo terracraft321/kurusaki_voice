de9341c7fb642152a8af1b0092a2780f587e5ee2
Terracraft <6617067+terracraft321@users.noreply.github.com>
2018-12-30 16:52:37 +0100

    Update audio.py

--- audio.py
+++ audio.py
@@ -5,6 +5,28 @@ from discord.ext import commands
 from discord.utils import find
 import requests as rq
 
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+opts = {
+    'default_search': 'auto',
+    'quiet': True,
+}
 
 def get_prefix(bot, msg):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
